597556738b02731d80f7819248c05266da437b0f
Charlie Hoover <choover@underarmour.com>
2015-12-17 11:39:05 -0500

    Adding package.json

--- index.js
+++ index.js
@@ -4,23 +4,17 @@
 // http://fredkschott.com/post/2014/06/require-and-the-module-system/
 var Module = require('module');
 
-var hijackedRequireMap = {};
+var cache = {};
 
 Module.prototype.__origRequire = Module.prototype.require;
-
 
 Module.prototype.require = function (path) {
 	var resolvedPath = Module._resolveFilename(path, this);
 
-	if (hijackedRequireMap[resolvedPath]) {
-		return hijackedRequireMap[resolvedPath];
-	}
-	return this.__origRequire(path);
+	return cache[resolvedPath] ? cache[resolvedPath] : this.__origRequire(path);
 };
 
 // hijackRequire
 Module.prototype.hijackRequire = function (path, fn) {
-	var resolvedPath = Module._resolveFilename(path, this);
-
-	hijackedRequireMap[resolvedPath] = fn();
+	cache[Module._resolveFilename(path, this)] = fn();
 };
